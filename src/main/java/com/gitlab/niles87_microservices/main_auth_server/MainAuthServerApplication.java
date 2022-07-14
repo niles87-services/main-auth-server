ee21df18b1503cb86cf6f23bb3a473bf9bcc7edb
Niles Bingham <nbing19@gmail.com>
2022-07-13 19:52:34 -0700

    initial files

---
+++ src/main/java/com/gitlab/niles87_microservices/main_auth_server/MainAuthServerApplication.java
@@ -0,0 +1,14 @@
+package com.gitlab.niles87_microservices.main_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainAuthServerApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		SpringApplication.run(MainAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlab/niles87_microservices/main_auth_server/MainAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.niles87_microservices.main_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
