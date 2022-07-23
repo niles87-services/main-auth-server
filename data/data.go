45202e7189f3e6b12b94828a094f5fcc69a613bb
Niles Bingham <nbing19@gmail.com>
2022-07-23 11:53:01 -0700

    added json tags to userdto

--- data/data.go
+++ data/data.go
@@ -12,9 +12,9 @@ type User struct {
 }
 
 type UserDto struct {
-	Id    int64
-	Name  string
-	Email string
+	Id    int64  `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
 }
 
 type UpdateUserDto struct {
